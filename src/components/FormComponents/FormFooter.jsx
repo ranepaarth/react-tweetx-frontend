0d03b6b3b2f772cbc98af6b3b80c0d677f24d134
ranepaarth <2019paarth.rane@ves.ac.in>
2024-02-07 13:18:33 +0530

    fix: :fire: remove FormFooter and HomePage

--- src/components/FormComponents/FormFooter.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const FormFooter = ({ text, path }) => {
-  return <Link to={path} className="text-sm capitalize font-medium text-neutral-600 hover:underline">{text}</Link>;
-};
-
-export default FormFooter;
