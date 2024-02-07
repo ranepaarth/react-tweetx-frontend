c8fb5d34f1c3e9046dd606df87f4b87461d15f30
ranepaarth <2019paarth.rane@ves.ac.in>
2024-02-07 13:34:23 +0530

    chore: :lipstick: layout for app and user forms
    create layouts for our main app after login and for login, register and reset password pages. This also includes protected routing based on the user status.

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "../components/NavBar/Navbar";
+import { getUserData } from "../slice/usersSlice";
 
 const AppLayout = () => {
+  const currentUser = useSelector(getUserData);
   return (
     <div className="bg-neutral-100 min-h-screen">
       <Navbar />
 
-      <main className="px-[5%] md:px-[10%] h-[calc(100dvh-72px)]">
-        <Outlet />
+      <main className="px-[5%] md:px-[10%] h-[calc(100dvh-72px)] py-5 flex justify-center">
+        {currentUser ? <Outlet /> : <Navigate to="/user/login" replace />}
       </main>
 
       <footer className="bg-neutral-200 w-full py-1 flex items-center justify-center text-xs text-neutral-400">
--- src/layouts/LoginRegisterLayout.jsx
+++ src/layouts/LoginRegisterLayout.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 import LogoText from "../components/NavBar/LogoText";
+import { getUserData } from "../slice/usersSlice";
 
 const LoginRegisterLayout = () => {
+  const currentUser = useSelector(getUserData);
   return (
     <div className="p-[5%] md:px-[10%]">
       <nav className="mb-8">
         <LogoText />
       </nav>
       <main>
-        <Outlet />
+        <Toaster />
+        {!currentUser ? <Outlet /> : <Navigate to="/" />}
       </main>
-    </div> 
+    </div>
   );
 };
 
