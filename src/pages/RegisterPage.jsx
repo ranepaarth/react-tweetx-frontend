089d457dca220c11dac99aecc2518ab2857eb917
ranepaarth <2019paarth.rane@ves.ac.in>
2024-02-07 15:43:55 +0530

    feat: user register
    create a user register page with input validations and error handling

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
-import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import FormBody from "../components/FormComponents/FormBody";
 import FormContainer from "../components/FormComponents/FormContainer";
 import FormHeader from "../components/FormComponents/FormHeader";
 import FormInput from "../components/FormComponents/FormInput";
 import FormNavigationButton from "../components/FormComponents/FormNavigationButton";
+import FormPasswordInput from "../components/FormComponents/FormPasswordInput";
 import FormRow from "../components/FormComponents/FormRow";
 import FormSubmitButton from "../components/FormComponents/FormSubmitButton";
+import { useRegisterUserMutation } from "../slice/authApiSlice";
+import { formValidation } from "../utils/formValidationOptions";
 
 const RegisterPage = () => {
-  const [showPwd, setShowPwd] = useState(false);
-  const toggleShowPwd = () => {
-    setShowPwd((prev) => !prev);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const [registerUser, { isLoading, isSuccess, error }] =
+    useRegisterUserMutation();
+
+  const onFormSubmit = async (data) => {
+    const response = await registerUser(data).unwrap();
+    toast.success("Account created successfully", {
+      duration: 5000,
+    });
+    // console.log(response);
+    reset();
+  };
+
+  const onFormError = (error) => {
+    // console.log(error);
   };
   return (
     <>
@@ -21,27 +44,50 @@ const RegisterPage = () => {
           <div className="mb-10">
             <FormHeader heading={"create account"} />
           </div>
-          <FormBody>
-            <FormRow>
-              <FormInput placeholder={"name"} type={"text"} autoFocus={true} />
-            </FormRow>
-            <FormRow>
-              <FormInput placeholder={"email"} type={"text"} />
-            </FormRow>
-            <FormRow>
+          <FormBody
+            handleSubmit={handleSubmit}
+            onFormError={onFormError}
+            onFormSubmit={onFormSubmit}
+          >
+            <FormRow error={errors?.name?.message}>
               <FormInput
-                placeholder={"password"}
-                type={showPwd ? "text" : "password"}
+                placeholder={"name"}
+                type={"text"}
+                autoFocus={true}
+                register={register("name", formValidation.name)}
               />
-              <button onClick={toggleShowPwd}>
-                {showPwd ? <IoEyeOffOutline /> : <IoEyeOutline />}
-              </button>
             </FormRow>
-            <FormRow>
-              <FormInput placeholder={"confirm password"} type={"password"} />
+            <FormRow error={errors?.email?.message || error?.data?.message}>
+              <FormInput
+                placeholder={"email"}
+                type={"text"}
+                register={register("email", formValidation.email)}
+              />
             </FormRow>
-            <div className="flex justify-end w-full">
-              <FormSubmitButton text={"Sign up"} />
+            <FormRow error={errors?.password?.message}>
+              <FormPasswordInput
+                register={register("password", formValidation.password)}
+              />
+            </FormRow>
+            <FormRow error={errors?.confirmPassword?.message}>
+              <FormInput
+                placeholder={"confirm password"}
+                type={"password"}
+                register={register("confirmPassword", {
+                  ...formValidation.confirmPassword,
+                  validate: (value) => {
+                    if (watch("password") !== value)
+                      return "Those passwords did'nt match. Try again";
+                  },
+                })}
+              />
+            </FormRow>
+            <div className="flex justify-between items-center w-full">
+              <FormSubmitButton
+                text={"Create account"}
+                isLoading={isLoading}
+                isSuccess={isSuccess}
+              />
             </div>
           </FormBody>
         </FormContainer>
