fd9f1fc7c264d2d1ead0fe4e18ba1860afda1861
ranepaarth <2019paarth.rane@ves.ac.in>
2024-02-27 23:20:32 +0530

    feat: :sparkles: implement feed page
    implement feed page to display all tweets of the user as well of the following users.

---
+++ src/components/FeedPage/SingleTweet.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useGetSingleUserQuery } from "../../slice/authApiSlice";
+import UserAvatar from "../shared/UserAccount/UserAvatar";
+import UserName from "../shared/UserAccount/UserName";
+import TweetContent from "./TweetContent";
+import TweetDeleteBtn from "./TweetDeleteBtn";
+import TweetEditBtn from "./TweetEditBtn";
+import { tweetCreatedAt } from "../../utils/utils";
+
+const SingleTweet = ({ tweet, currUserId }) => {
+  const tweetCreated = tweetCreatedAt(tweet?.createdAt);
+  const { data: singleUser } = useGetSingleUserQuery(tweet?.userId);
+
+  return (
+    <article
+      className={`w-full bg-white shadow-md shadow-neutral-200/50 rounded pl-4 py-4 flex items-center justify-between max-h- overflow-hidden relative ${
+        tweet?._id ? " block" : "hidden"
+      }`}
+    >
+      <div className="w-full flex items-center">
+        <div className="w-full flex items-start gap-6">
+          <UserAvatar userName={singleUser?.name} className={"avatar w-16"} />
+          <div className="flex flex-col gap-y-1 w-full">
+            <UserName
+              userName={singleUser?.name}
+              className={
+                "text-xl font-bold text-neutral-500 hover:text-neutral-800 transition-colors ease-in-out duration-200 cursor-pointer"
+              }
+            />
+            <TweetContent content={tweet?.content} />
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-2 right-10 flex items-center gap-2">
+        <TweetEditBtn tweet={tweet} currUserId={currUserId} />
+        <p className="text-xs text-neutral-500 font-medium">{tweetCreated}</p>
+
+        {currUserId === tweet?.userId ? (
+          <TweetDeleteBtn tweetId={tweet?._id} />
+        ) : (
+          ""
+        )}
+      </div>
+      <div className=" w-[30px] h-[60px] aspect-video rounded-tl-full rounded-bl-full bg-pink-500/70 pl-4"></div>
+    </article>
+  );
+};
+
+export default SingleTweet;
--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import CreateTweetBtn from "../components/FeedPage/CreateTweetBtn";
+import SingleTweet from "../components/FeedPage/SingleTweet";
+import TweetLoadingSkeleton from "../components/FeedPage/TweetLoadingSkeleton";
+import TweetModal from "../components/Modals/TweetModal";
+import LogoText from "../components/NavBar/LogoText";
+import NoTweets from "../components/ProfilePage/ProfileSection/NoTweets";
+import { useGetAllTweetsQuery } from "../slice/authApiSlice";
+import { getShowModalStatus, getUserData } from "../slice/usersSlice";
 
 const FeedPage = () => {
-  return (
-    <div>
-      Feed
-    </div>
-  )
-}
+  const currUser = useSelector(getUserData);
+  const { data: allTweets, isLoading } = useGetAllTweetsQuery(currUser?.id);
+  const showModal = useSelector(getShowModalStatus);
+  // console.log(allTweets);
 
-export default FeedPage
+  let content;
+  if (allTweets?.length === 0) content = <NoTweets />;
+  if (isLoading) content = <TweetLoadingSkeleton />;
+  if (allTweets?.length > 0) {
+    content = allTweets?.map((tweet, index) => (
+      <div key={index} className="w-full">
+        <SingleTweet tweet={tweet} currUserId={currUser?.id} />
+      </div>
+    ));
+  }
+
+  // console.log(showModal);
+  return (
+    <div className="flex flex-col gap-y-8 items-start justify-start w-full">
+      <div className="flex items-center justify-between w-full mb-4">
+        <CreateTweetBtn />
+        <LogoText />
+      </div>
+      {showModal ? <TweetModal /> : ""}
+      {content}
+    </div>
+  );
+};
+
+export default FeedPage;
