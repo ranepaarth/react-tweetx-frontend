4e61d4c35a4101e427570884da4ac3a829d47c5e
ranepaarth <2019paarth.rane@ves.ac.in>
2024-02-27 22:45:29 +0530

    feat: :sparkles: display all users
    a page to display all the users along with their basic information and a button to follow or unfollow the user

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import UserLoadingSkeleton from "../components/Loading/UserLoadingSkeleton";
+import LogoText from "../components/NavBar/LogoText";
+import NoUsers from "../components/UsersPage/NoUsers";
+import SingleUser from "../components/UsersPage/SingleUser";
+import { useGetAllUsersQuery } from "../slice/authApiSlice";
+import { getUserData } from "../slice/usersSlice";
 
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  const currUser = useSelector(getUserData);
+  const { data: allUsers, isLoading } = useGetAllUsersQuery(undefined, {
+    refetchOnMountOrArgChange: false,
+  });
+
+  let content;
+  if (isLoading) content = <UserLoadingSkeleton />;
+
+  if (!isLoading && allUsers?.length === 0) {
+    content = allUsers?.length === 0 && <NoUsers />;
+  }
+
+  if (!isLoading && allUsers?.length > 0) {
+    content = allUsers?.map((user, i) => (
+      <React.Fragment key={user?._id}>
+        {currUser?.id === user?._id ? (
+          ""
+        ) : (
+          <SingleUser key={user?._id} userId={user._id} user={user} />
+        )}
+      </React.Fragment>
+    ));
+  }
+
+  return (
+    <>
+      <div className="flex items-center w-full justify-between gap-10">
+        <span className="md:hidden block">
+          <LogoText />
+        </span>
+      </div>
+      <section className="flex flex-col items-center mt-5">{content}</section>
+    </>
+  );
 };
 
 export default UsersPage;
