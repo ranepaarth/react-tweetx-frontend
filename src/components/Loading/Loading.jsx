eec929edaf41c8ddcbb0d6e50f642c41d8995903
ranepaarth <2019paarth.rane@ves.ac.in>
2024-02-27 23:30:25 +0530

    style: :lipstick: suspense fallback loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-full grid place-items-center h-screen">
+      <span className="loading"></span>
+    </div>
+  );
+};
+
+export default Loading;
