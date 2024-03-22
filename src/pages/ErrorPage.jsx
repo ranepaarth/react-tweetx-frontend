a57f8ccacba11270ca401f4dda547c67102b3ab0
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-23 04:48:09 +0530

    fix: :art: custom error page
    error page is displayed whenever user tries to access any non existing route. It uses the stored acces token for redirecting user

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getAccessToken } from "../features/slice/authSlice";
 
 const ErrorPage = () => {
+  const accessToken = useSelector(getAccessToken);
+
   return (
     <section className="min-h-full flex flex-col items-center justify-center">
       <span className="font-extrabold text-5xl my-4">404</span>
       <p className="text-center text-xs px-14">
         Sorry we couldn't find this page. But don't worry, you can find plenty
-        of other things on our{" "}
-        <Link to="/" className="text-pink-500 px-1 hover:underline">
-          homepage
+        of other things {accessToken ? " on our " : " once "}
+        <Link
+          to={!accessToken ? "/auth/login" : "/"}
+          className="text-pink-500 px-1 hover:underline"
+        >
+          {!accessToken ? "Login" : "homepage"}
         </Link>
       </p>
     </section>
