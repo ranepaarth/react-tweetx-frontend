ed8eb328cf94b4607cbf4a74c438d35e2fa0f35e
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-23 04:53:47 +0530

    refactor: :art: dynamic toast position based on screen size

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,18 +1,44 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Toaster } from "react-hot-toast";
+import { Outlet } from "react-router-dom";
 import MobileNav from "../components/NavBar/MobileNav";
 import Navbar from "../components/NavBar/Navbar";
-import { getUserData } from "../slice/usersSlice";
 
 const AppLayout = () => {
-  const currentUser = useSelector(getUserData);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="min-h-screen bg-neutral-50 flex flex-col justify-between items-center md:justify-start h-fit">
       <Navbar />
 
-      <main className="px-[5%] md:px-0 my-2 py-4 md:py-6 w-full max-w-[600px]">
-        {currentUser ? <Outlet /> : <Navigate to="/user/login" replace />}
+      {isSmallScreen ? (
+        <Toaster
+          position="bottom-center"
+          containerStyle={{ bottom: 60 }}
+          toastOptions={{ duration: 2000 }}
+          gutter={10}
+        />
+      ) : (
+        <Toaster
+          position="top-right"
+          containerStyle={{ top: 70 }}
+          toastOptions={{ duration: 2000 }}
+          gutter={10}
+        />
+      )}
+      <main className="px-[3%] md:px-0 my-2 py-4 md:py-6 w-full max-w-[600px]">
+        <Outlet />
       </main>
 
       <MobileNav />
