b9bb6743f2c51393c3802bac6b25076a8c20aa02
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-23 04:24:25 +0530

    perf: :zap: create component to prefetch some data
    this prefetch component helps in fetching some necessary data before the app is rendered and thus imporves the overall user experience

---
+++ src/components/PrefetchComponent.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { tweetsApiSlice } from "../features/api/tweetsApiSlice";
+import { usersApiSlice } from "../features/api/usersApiSlice";
+import { store } from "../store";
+
+const PrefetchComponent = () => {
+  useEffect(() => {
+    console.log("subscribing");
+    const users = store.dispatch(
+      usersApiSlice.endpoints.getAllUsers.initiate()
+    );
+
+    const tweets = store.dispatch(
+      tweetsApiSlice.endpoints.getFeedTweets.initiate()
+    );
+
+    const profile = store.dispatch(
+      usersApiSlice.endpoints.getCurrUserProfile.initiate()
+    );
+
+    const savedTweets = store.dispatch(
+      tweetsApiSlice.endpoints.getAllSavedTweets.initiate()
+    );
+
+    return () => {
+      console.log("unsubscribing");
+      users.unsubscribe();
+      tweets.unsubscribe();
+      profile.unsubscribe();
+      savedTweets.unsubscribe();
+    };
+  }, []);
+  return <Outlet />;
+};
+
+export default PrefetchComponent;
