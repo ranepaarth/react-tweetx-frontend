2dad417b1601a64427ba3131d363c579ff439cc6
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-23 04:36:08 +0530

    refactor: :art: imporve routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
+import Loading from "./components/Loading/Loading.jsx";
 import "./index.css";
 import { store } from "./store.js";
-import Loading from "./components/Loading/Loading.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Suspense fallback={<Loading />}>
     <React.StrictMode>
       <Provider store={store}>
-        <App />
-        {/* <Loading /> */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </React.StrictMode>
   </Suspense>
