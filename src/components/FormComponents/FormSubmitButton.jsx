5b64709508d0dc2e1b2b991fa6447fcfedfe26bf
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-24 03:39:27 +0530

    fix: :lipstick: disabled button styling

--- src/components/FormComponents/FormSubmitButton.jsx
+++ src/components/FormComponents/FormSubmitButton.jsx
@@ -9,8 +9,8 @@ const FormSubmitButton = ({ text, isLoading, isSuccess }) => {
   }
   return (
     <button
-      className="px-6 py-2 bg-pink-500/80 text-white font-medium text-sm rounded-md shadow-lg grid place-items-center w-40 disabled:bg-pink-800/40"
-      disabled={isSuccess && isLoading}
+      className="px-6 py-2 bg-pink-500/80 text-white font-medium text-sm rounded-md shadow-lg grid place-items-center w-40 disabled:bg-pink-800/60"
+      disabled={isLoading}
     >
       {content}
     </button>
