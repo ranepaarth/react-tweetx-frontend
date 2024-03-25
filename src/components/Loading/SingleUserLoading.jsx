da459f0b8a2a8ccda1a531f9f42ab26a1541a8a9
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-25 16:16:25 +0530

    feat: :lipstick: loading skeleton for single user

---
+++ src/components/Loading/SingleUserLoading.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SingleUserLoading = () => {
+  return (
+    <article className="w-full flex items-start justify-between animate-pulse px-3 py-6">
+      <div className="flex items-center gap-4">
+        <p className="w-[4.5rem] aspect-square rounded-full bg-neutral-300 p-1"></p>
+        <div className="flex flex-col gap-2">
+          <p className="w-32 aspect-auto bg-neutral-300 h-6"></p>
+          <p className="w-56 aspect-auto bg-neutral-300 h-5"></p>
+        </div>
+      </div>
+      <div>
+        <button className="w-24 h-8 bg-neutral-300 rounded"></button>
+      </div>
+    </article>
+  );
+};
+
+export default SingleUserLoading;
