e47dec4aa4049175490751a788132bdfc86dfd55
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-26 02:16:42 +0530

    fix: :lipstick: display tweet content inside disabled textarea
    use a library for textarea auto resize (react-textarea-autosize) depending on its content

--- src/components/FeedPage/TweetContent.jsx
+++ src/components/FeedPage/TweetContent.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import TextareaAutosize from "react-textarea-autosize";
 
 const TweetContent = ({ content }) => {
   return (
-    <div className="text-sm md:text-base text-neutral-700 py-1 flex flex-grow font-normal text- w-11/12">
-      <p className="line-clamp-">{content}</p>
+    <div className="text-sm md:text-base text-neutral-700 py-1 font-normal w-[90%]">
+      <TextareaAutosize
+        className="w-full resize-none text-wrap bg-transparent"
+        maxRows={10}
+        disabled
+        value={content}
+      />
     </div>
   );
 };
