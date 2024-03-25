fd295ab3a64905969f72290dcbe24776c312f902
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-25 16:06:36 +0530

    feat: :lipstick: modal to edit tweet and implement loading skeleton

--- src/pages/SavedTweetsPage.jsx
+++ src/pages/SavedTweetsPage.jsx
@@ -1,48 +1,45 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import TweetLoadingSkeleton from "../components/Loading/TweetLoadingSkeleton";
+import TweetModal from "../components/Modals/TweetModal";
 import NoSavedTweets from "../components/SavedTweetsPage/NoSavedTweets";
 import SavedTweet from "../components/SavedTweetsPage/SavedTweet";
-import {
-  useGetAllSavedTweetsQuery,
-  useSaveTweetMutation,
-} from "../features/api/tweetsApiSlice";
+import { useGetAllSavedTweetsQuery } from "../features/api/tweetsApiSlice";
 import { getCurrentUserId } from "../features/slice/authSlice";
+import { getShowTweetModal } from "../features/slice/tweetsSlice";
 import { getCurrentUser } from "../features/slice/usersSlice";
 
 const SavedTweetsPage = () => {
   const currentUserId = useSelector(getCurrentUserId);
   const currentUser = useSelector(getCurrentUser);
 
-  const [saveTweet] = useSaveTweetMutation();
-  const { data } = useGetAllSavedTweetsQuery();
+  const showTweetModal = useSelector(getShowTweetModal);
+  const { data, isLoading } = useGetAllSavedTweetsQuery();
 
-  const handleSaveTweet = async (tweetId) => {
-    try {
-      const result = await saveTweet({ tweetId });
-    } catch (error) {
-      console.log(error);
-    }
-  };
   let content;
   if (data?.currentUser?.savedTweets?.length === 0) {
     content = <NoSavedTweets />;
   }
   if (data?.currentUser?.savedTweets?.length > 0) {
     content = data?.currentUser?.savedTweets.map((tweet) => (
-      <SavedTweet
-        tweet={tweet}
-        key={tweet._id}
-        handleSaveTweet={handleSaveTweet}
-        currentUserId={currentUserId}
-      />
+      <SavedTweet tweet={tweet} key={tweet._id} currentUserId={currentUserId} />
     ));
+  }
+
+  if (isLoading) {
+    content = <TweetLoadingSkeleton />;
   }
 
   return (
     <section className="flex flex-col items-start gap-y-4 w-full">
+      {showTweetModal ? <TweetModal /> : ""}
       <header className="mb-4">
         <h2 className="font-bold text-2xl text-neutral-700">Bookmarks</h2>
-        <h4 className="text-neutral-500">@{currentUser?.userName}</h4>
+        {!currentUser?.userName ? (
+          <h4 className="bg-neutral-200 animate-pulse w-20 h-4 py-1 rounded"></h4>
+        ) : (
+          <h4 className="text-neutral-500">@{currentUser?.userName}</h4>
+        )}
       </header>
       {content}
     </section>
