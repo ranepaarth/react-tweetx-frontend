f1203fbd3b3cdae034a7216b4e097f73be98d35c
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-28 12:29:13 +0530

    fix: :bug: navigate to root on logo click
    when user click on logo and no access toke then it will redirect to login page

--- src/components/NavBar/LogoText.jsx
+++ src/components/NavBar/LogoText.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getAccessToken } from "../../features/slice/authSlice";
 
 const LogoText = () => {
-  return (
-    <Link className="font-bold text-pink-400 text-2xl" to='/'>TweetX</Link>
-  )
-}
+  const currentAccessToken = useSelector(getAccessToken);
+  return currentAccessToken ? (
+    <Link className="font-bold text-pink-400 text-2xl" to="/">
+      TweetX
+    </Link>
+  ) : (
+    <Link className="font-bold text-pink-400 text-2xl" to="/auth/login">
+      TweetX
+    </Link>
+  );
+};
 
-export default LogoText
+export default LogoText;
